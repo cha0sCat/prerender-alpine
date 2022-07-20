f0d007560d2442f21eaf2cf6a2b7620c242da691
Anonymous <>
2022-07-20 23:05:34 +0900

    fix: import path

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ if (memCache === 1) {
 server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 server.use(prerender.removeScriptTags());
-server.use(require('blockResource'));
+server.use(require('./blockResource'));
 
 server.start();
