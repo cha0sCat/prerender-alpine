db4782fd2e39ce53f06d4932bd271bccce868a87
Anonymous <>
2022-07-27 15:54:08 +0900

    feat: 记录请求来源 ua

--- logUserAgent.js
+++ logUserAgent.js
@@ -2,6 +2,7 @@ module.exports = {
     requestReceived: (req, res, next) => {
         // 只是记录请求 ua，不做任何修改
         // 用于查看谁在爬我
-        console.log(req.headers['user-agent']);
+        console.log("Request User-Agent: ", req.headers['user-agent']);
+        next()
     }
 };
